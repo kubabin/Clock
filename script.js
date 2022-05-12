086b6a0dc8b409d68f345ab98cf007c2dda0bbf5
createview <58891993+kubabin@users.noreply.github.com>
2022-05-12 20:59:19 +0200

    create smooth hand moving

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 
-
-window.setInterval(function(){
-    d = new Date(); //object of date()
-    hr = d.getHours();
-    min = d.getMinutes();
-    sec = d.getSeconds();
-    hr_rotation = 30 * hr + min / 2; //converting current time
-    min_rotation = 6 * min;
-    sec_rotation = 6 * sec;
+    function runClock() {
+        const now = new Date();
+        const hour = now.getHours() % 12;
+        const min = now.getMinutes();
+        const sec = now.getSeconds();
+        const ms = now.getMilliseconds();
+        
+        const clock = document.querySelector('.clock');
+        const hourHand = clock.querySelector('.hours');
+        const minuteHand = clock.querySelector('.minutes');
+        const secondHand = clock.querySelector('.seconds');
+        
+        const hourRotation = 30 * hour + 0.5 * min;
+        const minRotation = 6 * min + 0.1 * sec;
+        const secRotation = 6 * sec + 0.006 * ms;
+        hourHand.style.transform = `rotate(${hourRotation}deg)`;
+        minuteHand.style.transform = `rotate(${minRotation}deg)`;
+        secondHand.style.transform = `rotate(${secRotation}deg)`;
+        
+        requestAnimationFrame(runClock)
+    };
     
-    document.querySelector(".hours").style.transform = `rotate(${hr_rotation}deg)`;
-    document.querySelector(".minutes").style.transform = `rotate(${min_rotation}deg)`;
-    document.querySelector(".seconds").style.transform = `rotate(${sec_rotation}deg)`;
-}, 1000)
+    runClock();
