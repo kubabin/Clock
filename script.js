56e5d2139b09da9850cfcdeff2bf02dfd3888e5d
createview <58891993+kubabin@users.noreply.github.com>
2022-05-12 18:58:10 +0200

    Main files

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-const hands = document.querySelectorAll('.hand')
 
-window.setInterval(function() {
-    const hours;
-})
+
+window.setInterval(function(){
+    d = new Date(); //object of date()
+    hr = d.getHours();
+    min = d.getMinutes();
+    sec = d.getSeconds();
+    hr_rotation = 30 * hr + min / 2; //converting current time
+    min_rotation = 6 * min;
+    sec_rotation = 6 * sec;
+    
+    document.querySelector(".hours").style.transform = `rotate(${hr_rotation}deg)`;
+    document.querySelector(".minutes").style.transform = `rotate(${min_rotation}deg)`;
+    document.querySelector(".seconds").style.transform = `rotate(${sec_rotation}deg)`;
+}, 1000)
